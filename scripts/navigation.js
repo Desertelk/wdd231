1a29854c97f8b985937c623b7512ada8efc4da38
Desertelk <cadenwatts111@gmail.com>
2026-01-10 00:54:50 -0500

    Portfolio changes

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -3,3 +3,9 @@ const navbutton = document.querySelector('#ham-btn');
 navbutton.addEventListener('click', () => {
     navbutton.classList.toggle('show');
 })
+
+const yearElement = document.getElementById('currentYear');
+const modifiedElement = document.getElementById('lastModified');
+
+yearElement.textContent = new Date().getFullYear();
+modifiedElement.textContent = document.lastModified;
