4b8e2b4cd7013f41e284bf1e9812c25e61237e51
Desertelk <cadenwatts111@gmail.com>
2026-01-29 21:57:45 -0500

    Directory page

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -44,7 +44,7 @@ hamBtn.addEventListener('click', () => {
 const town = document.querySelector('#town');
 const myTemperature = document.querySelector('#temperature');
 const myDescription = document.querySelector('#description');
-const high = document.querySelector('#description');
+const high = document.querySelector('#high');
 const low = document.querySelector('#low');
 const humidity = document.querySelector('#humidity');
 const sunrise = document.querySelector("#sunrise");
@@ -98,28 +98,101 @@ async function apiFetchWeather() {
 apiFetchWeather();
 
 function displayWeather(data){
-    myDescription.innerHTML = `${data.weather[0].myDescription}`;
+    myDescription.innerHTML = `${data.weather[0].description}`;
     myTemperature.innerHTML = `${data.main.temp} &degF`;
     high.innerHTML = `High: ${data.main.temp_max} &degF`;
-    high.innerHTML = `Low: ${data.main.temp_min} &degF`;
+    low.innerHTML = `Low: ${data.main.temp_min} &degF`;
     humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
     const timestampRise = data.sys.sunrise;
     const riseDate = new Date(timestampRise * 1000);
-    const riseTime = riseDate.toLocaleTimeString("en-US", {hour: "numeric"}, {minute: "2-digit"});
+    const riseTime = riseDate.toLocaleTimeString("en-US", {
+        hour: "numeric", minute: "2-digit"
+    });
     sunrise.innerHTML = `Sunrise: ${riseTime}`;
-    const timestampeSet = data.sys.
+    const timestampSet = data.sys.sunset;
+    const setDate = new Date(timestampSet * 1000);
+    const setTime = setDate.toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "2-digit"
+    });
+    sunset.innerHTML = `Sunset: ${setTime}`;
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    myGraphic.setAttribute('src', iconsrc);
+    myGraphic.setAttribute('alt', data.weather[0].description);
 }
+
+async function apiFetchForecast() {
+    try {
+        const response = await fetch(forecastUrl);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            // console.table(data);
+            // displayResults(data); // uncomment when ready
+            displayForecast(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayForecast(data) {
+    const timestampTwoDays = data.list[16].dt;
+    const twoDaysDate = new Date(timestampTwoDays * 1000);
+    const twoDays = twoDaysDate.toLocaleDateString("en-US", {
+        weekday: "long"
+    });
+    const timestampThreeDays = data.list[24].dt;
+    const threeDaysDate = new Date(timestampThreeDays * 1000);
+    const threeDays = threeDaysDate.toLocaleDateString("en-US", {
+        weekday: "long"
+    });
+    myToday.innerHTML = `Tomorrow: ${data.list[8].main.temp_max} &degF`;
+    myTomorrow.innerHTML = `${twoDays}: ${data.list[16].main.temp_max} &degF`;
+    myTwoDays.innerHTML = `${threeDays}: ${data.list[24].main.temp_max} &degF`;
+}
+
+apiFetchForecast();
 
 async function loadSpotlights(){
-    const response = await fetch("data/members.json");
-    const members = await response.json();
+    try {
 
-    const qualified = members.filter(
-        m => m.level === 2 || m.level === 3
-    );
+        const response = await fetch("data/members.json");
+        const members = await response.json();
+    
+        const qualified = members.filter(
+            m => m.level === 2 || m.level === 3
+        );
+    
+        qualified.sort(() => .5 - Math.random());
+    
+        const selected = qualified.slice(0,3);
+        const container = document.querySelector("#spotlight-container");
+        container.innerHTML = "";
+    
+        selected.forEach(m => {
+            const card = document.createElement("section");
+            card.classList.add("card");
 
-    qualified.sort(() => .5 - Math.random());
-
-    const selected = qualified.slice(0,3);
-    const container = document.querySelector("#spotlight-container");
+            const badgeText = m.level === 3 ? "Gold Member" : "Silver Member";
+            const badgeClass = m.leve === 3 ? "gold" : "silver";
+    
+            card.innerHTML = `
+                
+                <h3>${m.name}</h3>
+                <img src="images/${m.image}" alt="${m.image}">
+                <p><span class="badge ${badgeClass}">${badgeText}</span></p>
+                <p>${m.address}</p>
+                <p>${m.phone}</p>
+                <a href="${m.website}" target="_blank">Visit Website</a>
+            `;
+            container.appendChild(card);
+        });
+    } catch (error) {
+        console.error("Error loading spotlights: ", error);
+    }    
 }
+
+loadSpotlights();
