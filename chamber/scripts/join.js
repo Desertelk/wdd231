005b505d85ab9207b71e153e3bc0a211483c94ba
Desertelk <cadenwatts111@gmail.com>
2026-02-02 21:17:58 -0500

    Join page creation

---
+++ chamber/scripts/join.js
@@ -0,0 +1,52 @@
+const yearElement = document.getElementById('currentYear');
+const modifiedElement = document.getElementById('lastModified');
+
+yearElement.textContent = new Date().getFullYear();
+modifiedElement.textContent = document.lastModified;
+
+const navItems = [
+    {name: "Home", url: "index.html"},
+    {name: "Directory", url: "directory.html"},
+    {name: "Join", url: "join.html"},
+    {name: "Discover", url: "#"}
+];
+
+const nav = document.getElementById("main-nav");
+const ul = document.createElement("ul");
+navItems.forEach(item => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+
+    a.textContent = item.name;
+    a.href = item.url;
+
+    if (window.location.pathname.includes(item.url)){
+        a.classList.add("active");
+    }
+
+    li.appendChild(a);
+    ul.appendChild(li);
+});
+
+nav.appendChild(ul);
+
+const hamBtn = document.querySelector('#ham-btn');
+const navMenu = document.querySelector("#main-nav ul");
+
+hamBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+    hamBtn.classList.toggle('show');
+})
+
+document.getElementById("timestamp").value = new Date().toISOString();
+
+document.querySelector("[data-modal]").forEach(link => {
+    link.addEventListener("click", e => {
+        e.preventDefault();
+        document.getElementById(link.dataset.modal).showModal();
+    });
+});
+
+document.querySelectorAll("dialog button").forEach(btn => {
+    btn.addEventListener("click", () => btn.closest("dialog").close());
+});
