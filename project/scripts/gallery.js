3698fbb6a41b36e790500a6b8a9bb927439a38d1
Desertelk <cadenwatts111@gmail.com>
2026-02-18 21:09:58 -0500

    Final Project

---
+++ project/scripts/gallery.js
@@ -0,0 +1,154 @@
+const yearElement = document.getElementById('currentYear');
+const modifiedElement = document.getElementById('lastModified');
+
+if (yearElement) yearElement.textContent = new Date().getFullYear();
+if (modifiedElement) modifiedElement.textContent = document.lastModified;
+
+const navItems = [
+    {name: "Home", url: "index.html"},
+    {name: "Services", url: "services.html"},
+    {name: "Contact", url: "contact.html"},
+    {name: "Gallery", url: "gallery.html"}
+];
+
+const nav = document.getElementById("main-nav");
+
+if (nav){
+    const ul = document.createElement("ul");
+    const currentPage = window.location.pathname.split("/").pop() || "index.html";
+    navItems.forEach(item => {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+    
+        a.textContent = item.name;
+        a.href = item.url;
+    
+    
+        if (currentPage === item.url){
+            a.classList.add("active");
+        }
+    
+        li.appendChild(a);
+        ul.appendChild(li);
+    });
+    nav.appendChild(ul);
+}
+
+
+const hamBtn = document.querySelector('#ham-btn');
+const navMenu = document.querySelector("#main-nav ul");
+
+hamBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+    hamBtn.setAttribute("aria-expanded", navMenu.classList.contains("show"));
+    hamBtn.classList.toggle('show');
+})
+
+const galleryImages = [
+    {
+        src: "/project/images/courtyard-gabion-wall.webp",
+        alt: "Courtyard Gabion wall",
+        category: "Gabion Wall",
+        description: "Exterior Courtyard Gabion Wall"
+    },
+    {
+        src: "/project/images/exterior-steel-cladding.webp",
+        alt: "Exterior steel cladding",
+        category: "Cladding",
+        description: "Exterior steel cladding on residence"
+    },
+    {
+        src: "/project/images/gabion-wall-firebox.webp",
+        alt: "Exterior Gabion Wall made Fire box",
+        category: "Gabion Wall",
+        description: "Exterior Gabion Wall made Fire box"
+    },
+    {
+        src: "/project/images/gabion-wall-island.webp",
+        alt: "Exterior Gabion Wall island",
+        category: "Gabion Wall",
+        description: "Exterior Gabion wall made Island"
+    },
+    {
+        src: "/project/images/glass-steel-railing.webp",
+        alt: "Glass and Steel Railing",
+        category: "Stair & Railing",
+        description: "Interior railing made of Glass panels and Steel bars"
+    },
+    {
+        src: "/project/images/interior-gabion-wall.webp",
+        alt: "Interior Gabion Wall",
+        category: "Gabion Wall",
+        description: "Internal Gabion Wall"
+    },
+    {
+        src: "/project/images/interior-steel-cladding.webp",
+        alt: "Interior Steel Cladding",
+        category: "Cladding",
+        description: "Interior Steel Cladding"
+    },
+    {
+        src: "/project/images/interior-wooden-spiral-staircase.webp",
+        alt: "Interior Wooden Spiral Staircase",
+        category: "Stair & Railing",
+        description: "Interior Wooden Spiral Staircase"
+    },
+    {
+        src: "/project/images/steel-bar-rails.webp",
+        alt: "Exterior Steel Bar Rails",
+        category: "Stair & Railing",
+        description: "Exterior Steel Bar Rails"
+    },
+    {
+        src: "/project/images/wood-gate-gabion-wall.webp",
+        alt: "Exterior Wooden gate and Gabion Wall",
+        category: "Gabion Wall",
+        description: "Wooden Gate and Gabion Wall"
+    },
+]
+
+const galleryContainer = document.getElementById("gallery");
+
+if (galleryContainer){
+    galleryImages.forEach(img => {
+        const figure = document.createElement("figure");
+        figure.classList.add("gallery-item");
+
+        const image = document.createElement("img");
+        image.src = img.src;
+        image.alt = img.alt;
+        image.loading = "lazy";
+
+        const caption = document.createElement("figcaption");
+        caption.textContent = img.category;
+
+        figure.appendChild(image);
+        figure.appendChild(caption);
+        galleryContainer.appendChild(figure);
+    });
+}
+
+const modal = document.createElement("dialog");
+modal.classList.add("image-modal");
+document.body.appendChild(modal);
+
+galleryContainer.querySelectorAll("img").forEach((img, index) => {
+
+    img.addEventListener("click", () => {
+
+        const data = galleryImages[index];
+
+        modal.innerHTML = `
+            <img src="${data.src}" alt="${data.alt}">
+            <h3>${data.category}</h3>
+            <p>${data.description}</p>
+            <button class="cta">Close</button>
+        `;
+
+        modal.querySelector("button").addEventListener("click", () => {
+            modal.close();
+        });
+
+        modal.showModal();
+    });
+});
---
+++ project/scripts/index.js
@@ -0,0 +1,100 @@
+const yearElement = document.getElementById('currentYear');
+const modifiedElement = document.getElementById('lastModified');
+
+if (yearElement) yearElement.textContent = new Date().getFullYear();
+if (modifiedElement) modifiedElement.textContent = document.lastModified;
+
+const navItems = [
+    {name: "Home", url: "index.html"},
+    {name: "Services", url: "services.html"},
+    {name: "Contact", url: "contact.html"},
+    {name: "Gallery", url: "gallery.html"}
+];
+
+const nav = document.getElementById("main-nav");
+
+if (nav){
+    const ul = document.createElement("ul");
+    const currentPage = window.location.pathname.split("/").pop() || "index.html";
+    navItems.forEach(item => {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+    
+        a.textContent = item.name;
+        a.href = item.url;
+    
+    
+        if (currentPage === item.url){
+            a.classList.add("active");
+        }
+    
+        li.appendChild(a);
+        ul.appendChild(li);
+    });
+    nav.appendChild(ul);
+}
+
+
+const hamBtn = document.querySelector('#ham-btn');
+const navMenu = document.querySelector("#main-nav ul");
+
+hamBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+    hamBtn.setAttribute("aria-expanded", navMenu.classList.contains("show"));
+    hamBtn.classList.toggle('show');
+})
+
+const imageContainer = document.getElementById("random-images");
+
+if (imageContainer) {
+    const images = [
+        "/project/images/courtyard-gabion-wall.webp",
+        "/project/images/exterior-steel-cladding.webp",
+        "/project/images/interior-wooden-spiral-staircase.webp",
+        "/project/images/steel-bar-rails.webp",
+        "/project/images/wood-gate-gabion-wall.webp",
+        "/project/images/interior-steel-cladding.webp",
+        "/project/images/gabion-wall-island.webp",
+        "/project/images/glass-steel-railing.webp",
+    ];
+
+    const shuffled = images.sort(() => 0.5 - Math.random());
+
+    const selected = shuffled.slice(0,3);
+
+    selected.forEach(src => {
+        const img = document.createElement("img");
+        img.src = src;
+        img.alt = "Custom stair project by JD Stairs";
+        img.loading = "lazy";
+        imageContainer.appendChild(img);
+    });
+}
+
+const form = document.querySelector(".contact-form");
+
+if (form) {
+    form.addEventListener("submit", (e) => {
+        const email = form.querySelector('input[name="email"]').value.trim();
+        const name = form.querySelector('input[name="name"').value.trim();
+        const project = form.querySelector('select[name="project"]').value;
+
+        if (name.length < 2){
+            alert("Please eneter your full name.");
+            e.preventDefault();
+            return;
+        }
+
+        if (!email.includes("@") || !email.includes(".")) {
+            alert("Please enter a valid email address.");
+            e.preventDefault();
+            return;
+        }
+
+        if (project === "") {
+            alert("Please select a project type.");
+            e.preventDefault();
+            return;
+        }
+    })
+}
